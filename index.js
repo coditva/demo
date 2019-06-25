d5251333f3d5277b2575adeb4ddcc80e61feb939
Utkarsh Maheshwari <coditva@gmail.com>
2019-06-25 23:26:59 +0530

    Send 'OK' on request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('OK');
 });
 server.listen(5000, function () {
     console.log('listening');
