41fb09bd7e0ce444d9377f3da9f075fbf25a9ba6
Utkarsh Maheshwari <coditva@gmail.com>
2019-06-26 00:23:54 +0530

    Chain listen to createServer

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('OK');
-});
-
-server.listen(5000, function () {
+}).listen(5000, function () {
     console.log('listening');
 });
