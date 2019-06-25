24abce652ce485775cdd08184b7a19167940eb43
Utkarsh Maheshwari <coditva@gmail.com>
2019-06-26 00:10:16 +0530

    Use ExpressJS module

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const express = require('express');
 
 const server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('OK');
 });
+
 server.listen(5000, function () {
     console.log('listening');
 });
