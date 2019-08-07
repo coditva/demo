264bab39707649ede444c151ef2e53306774824c
Utkarsh Maheshwari <coditva@gmail.com>
2019-08-07 22:20:40 +0530

    comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // a simple server
 const server = http.createServer(function (request, response) {
+    // write response
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('OK');
 }).listen(5000, function () {
